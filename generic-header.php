5ccb5f33299ed32d372abca2f174a5d939fda1dd
Josh Schoep <joshuaschoep@gmail.com>
2019-11-27 23:16:39 -0700

    Began work on header template

---
+++ generic-header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="header.css" type="text/css">
+<header>
+    <table>
+        <tr>
+            <td></td>
+            <td><img src="images/Logo.png" height="64px"></td>
+            <td><p>Logged in as Josh Schoep</p></td>
+        </tr>
+    </table>
+    <nav>
+        <ul>
+            <li><a href="/login">Sign in</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <head>
     <title>Final Project</title>
+    <link rel="icon" href="../images/Logo.png">
+    <link rel="stylesheet" href="./styles.css" type="text/css">
 </head>
 <body>
-    <p>Hello world!</p>
+    <?php include 'generic-header.php';?>
 </body>
