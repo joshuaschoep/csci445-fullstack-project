7fcb4c5eefb5a80367343c64106639477976c6a7
Josh Schoep <joshuaschoep@gmail.com>
2019-12-04 13:34:43 -0700

    Added search bar to header

---
+++ user/index.php
@@ -0,0 +1,7 @@
+<head>
+    <title>{{user}} on Resum&eacute;</title>
+    <link rel="stylesheet" href="users.css" type="text/css">
+</head>
+<body>
+    <?php include '../generic-header.php'?>     
+</body>
