558a2f377fc6f0e90716cf6386458667b31224cd
Josh Schoep <joshuaschoep@gmail.com>
2019-12-03 18:53:39 -0700

    Push so that Nick can have the current version

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <head>
     <title>Final Project</title>
-    <link rel="icon" href="../images/Logo.png">
+    <link rel="icon" href="./images/Logo.png">
     <link rel="stylesheet" href="./styles.css" type="text/css">
 </head>
 <body>
-    <?php include 'generic-header.php';?>
+    <article>
+        <section id="centerpiece">
+            <img src="./images/Logo.png" id="logo">
+            <h1>Welcome to Resum&eacute;</h1>
+        </section>
+    <article>
+    <form id="search">
+        <input type="text" id="text" placeholder="Search users">
+    </form>
 </body>
