3c0e8cc66e67f316251b302726f8cde4a2b48e00
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 10:39:30 -0700

    updated signup.php with username field, updated login placeholder with email/username. made signup_submit to issue email etc

--- login/signup.php
+++ login/signup.php
@@ -1,9 +1,12 @@
 <link rel="stylesheet" href="signup.css" type="text/css">
-<form id="register">
+<form id="register" action="./signup_submit.php" method="post">
     <fieldset>
         <h4>Register</h4>
         <div class="email">
             <label for="email"><i class="fas fa-at"></i></label><input type="email" name="email" placeholder="Email Address">
+        </div>
+        <div class="username">
+            <label for="username"><i class="fas fa-user"></i></label><input type="username" name="username" placeholder="User Name">
         </div>
         <div class="password">
             <label for="password"><i class="fas fa-lock"></i></label><input type="password" name="password" placeholder="Create Password">
