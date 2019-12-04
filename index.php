51d8081783013e9bcc310f315c27c4c43d957c89
Josh Schoep <joshuaschoep@gmail.com>
2019-12-04 10:35:19 -0700

    Added animations to login page

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <link rel="stylesheet" href="./styles.css" type="text/css">
 </head>
 <body>
-    <nav>
-        <a href="/login">Sign in</a>
-        <a id="signup" href="/login">Sign up</a>
-    </nav>
-    <article>
+    <div class="content">
+        <nav>
+            <a href="/login">Sign in</a>
+            <a id="signup" href="/login">Sign up</a>
+        </nav>
         <section id="centerpiece">
             <img src="./images/Logo.png" id="logo">
             <h1>Welcome to Resum&eacute;</h1>
@@ -17,5 +17,5 @@
                 <input type="text" placeholder="Search users"><input type="submit" value="Search">
             </form>
         </section>
-    </article>
+    </div>
 </body>
