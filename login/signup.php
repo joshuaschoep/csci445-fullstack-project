97f82ca379700d1817161d156a5066ccc5655002
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 11:39:52 -0700

    updated signup checking passwords match on submit. added sql file.

--- login/signup.php
+++ login/signup.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="signup.css" type="text/css">
-<form id="register" action="./signup_submit.php" method="post">
+<script src="./signup.js"></script>
+<form id="register" action="./signup_submit.php" method="post" onsubmit="return checkPasswordsMatch()">
     <fieldset>
         <h4>Register</h4>
         <div class="email">
@@ -9,13 +10,13 @@
             <label for="username"><i class="fas fa-user"></i></label><input type="username" name="username" placeholder="User Name">
         </div>
         <div class="password">
-            <label for="password"><i class="fas fa-lock"></i></label><input type="password" name="password" placeholder="Create Password">
+            <label for="password"><i class="fas fa-lock"></i></label><input id="pw" type="password" name="password" placeholder="Create Password">
         </div>
         <div class="repeat">
-            <label for="password"><i class="fas fa-redo"></i></label><input type="password" name="repeat" placeholder="Repeat Password">
+            <label for="password"><i class="fas fa-redo"></i></label><input id="pw-repeat" type="password" name="repeat" placeholder="Repeat Password">
         </div>
         <div class="submit">
-            <input type="submit" value="Submit"></input>
+            <input id="checkMatch" type="submit" value="Submit"></input>
         </div>
     </fieldset>
 </form>
---
+++ login/signup_submit.php
@@ -0,0 +1,15 @@
+<!-- email, username, password, repeat -->
+<?php 
+$email = $_POST["email"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$repeat = $_POST["repeat"];
+
+echo $email;
+echo $username;
+echo $password;
+echo $repeat;
+
+
+
+?>
