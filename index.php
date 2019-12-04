866872375ea69f8f73adae853bc3e76356c44ff5
Josh Schoep <joshuaschoep@gmail.com>
2019-12-03 23:05:41 -0700

    Fixed my mistakes :)))

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <link rel="stylesheet" href="./styles.css" type="text/css">
 </head>
 <body>
+    <a class="signin" href="/login">Sign in</a>
     <article>
         <section id="centerpiece">
             <img src="./images/Logo.png" id="logo">
             <h1>Welcome to Resum&eacute;</h1>
+            <form id="search">
+                <input type="text" placeholder="Search users"><input type="submit" value="Search">
+            </form>
         </section>
-    <article>
-    <form id="search">
-        <input type="text" placeholder="Search users"><input type="submit" value="Search">
-    </form>
+    </article>
 </body>
