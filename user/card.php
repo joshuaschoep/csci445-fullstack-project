19036bb95210d8704988e1e81ecf69ec0ecbf5b7
Josh Schoep <joshuaschoep@gmail.com>
2019-12-04 19:07:55 -0700

    Added optional edit and delete buttons to each card

--- user/card.php
+++ user/card.php
@@ -1,11 +1,14 @@
 <link rel="stylesheet" href="/user/card.css" type="text/css">
+<script src="https://kit.fontawesome.com/2a4e989807.js" crossorigin="anonymous"></script>
 <div class="card-wrapper">
-    <a href="">
+    <a href="#" class="edit"><i class="fas fa-edit"></i></a>
+    <a href="#" class="delete"><i class="fas fa-trash"></i></a>
+    <a href="/gohere">
     <article>
         <img class="label" src="/images/banner3.png" height="240px" width="360px">
         <section class="card-body">
             <h1>Title</h1>
-            <p>Describe me!</p>        
+            <p>Describe me!</p>
         </section>
     </article>
     </a>
