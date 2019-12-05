4f7da1e5405832ee1edfbc39917b4ad13b7ca649
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 20:53:17 -0700

    added check steps to registration

--- login/signup.js
+++ login/signup.js
@@ -1,3 +1,4 @@
+
 function checkPasswordsMatch() {
     // to quick check that user passwords match before submitting
     // does not check any password restrictions
@@ -15,5 +16,39 @@ function checkPasswordsMatch() {
         // hash the password on submit if it is acceptable
         // sha512 probably overkill but whatever
         document.getElementById("pw").value = hex_sha512(p1);
+        return true;
     }
+}
+
+function checkUserInfo() {
+    if (checkPasswordsMatch()) {
+        var uname = "";
+        uname = document.getElementById("uname").value;
+        
+        // query for usernames and if it exists reset the page
+        if (window.XMLHttpRequest) {
+            xht = new XMLHttpRequest();
+        } else {
+            xht = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    
+        xht.onreadystatechange=function() {
+            if(this.readyState==4 && this.status==200) {
+                if (this.responseText == "repeat") {
+                    // if the username is a repeat, return false
+                    alert("Username already taken. please try again");
+                    document.getElementById("uname").value = "";
+                    return false;
+                }
+            }
+        }
+        xht.open('GET', 'check_user.php?name='+uname,true);
+        xht.send();
+    }
+    else {
+        document.getElementById("pw").value = "";
+        document.getElementById("pw-repeat").value = "";
+        return false;
+    }
+
 }
---
+++ secrets.php
@@ -0,0 +1,8 @@
+<?php 
+$servername = 'localhost';
+$username = 'tlucas';
+$password = 'KIOKYMKU';
+$dbname = 'f19_tlucas';
+$domain = 'luna.mines.edu';
+$domain_path = 'fall_2019/tlucas';
+?>
