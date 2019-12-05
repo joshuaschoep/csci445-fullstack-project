abc0ae00df432ee54ff66da49aab26a1b0a2f5d2
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 19:05:39 -0700

    stupid ampersand

--- login/signup_verify.php
+++ login/signup_verify.php
@@ -17,7 +17,7 @@ if(isset($_GET["e"]) && !empty($_GET['e']) AND isset($_GET["h"]) && !empty($_GET
     $hash = mysqli_real_escape_string($conn, $_GET["h"]);
 
     // if email and hash match an entry, activate
-    $user_query = "SELECT user_id,hash FROM USERS WHERE email=$email";
+    $user_query = "SELECT user_id,hash FROM USERS WHERE email='$email'";
     $check_hash = $conn->query($user_query)->fetch_assoc()["hash"];
     $user_id = $conn->query($user_query)->fetch_assoc()["user_id"];
     echo 'check hash: ' . $check_hash . "<br>";
