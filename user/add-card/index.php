384bce455b866c9765cd6efecf3a1d87d270415d
Nicholas Todtenhagen <ntodtenhagen@protonmail.com>
2019-12-04 22:43:20 -0700

    Adding uid tracking through sessions

--- user/add-card/index.php
+++ user/add-card/index.php
@@ -1,3 +1,39 @@
+<?php
+session_start();
+
+if (!$_SESSION['logged_in']) {
+    echo "<script>window.location='../login/index.php';</script>";
+}
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'ntodtenhagen');
+define('DB_PASSWORD', 'CCAOSESG');
+define('DB_NAME', 'f19_ntodtenhagen');
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if (!$connection) {
+    die("DB Connection failed " . mysqli_connect_error());
+}
+
+if ($_GET['method'] === "update") {
+    $QUERY = $connection->prepare("SELECT * FROM WEBPAGEDATA WHERE card_id=?");
+    $QUERY->bind_param("i", $_GET["card_id"]);
+    if ($QUERY->execute) {
+        $row = $QUERY->get_result()->fetch_row();
+
+    }
+} else if ($_GET['method'] === "delete") {
+    $QUERY = $connection->prepare("DELETE FROM WEBPAGEDATA WHERE card_id=?");
+    $QUERY->bind_param("i", $_GET["card_id"]);
+    if($QUERY->execute) {
+        die();
+    }
+}
+
+?>
+
+
 <head>
     <title>Resum&eacute;</title>
     <link rel="stylesheet" href="add-card.css" type="text/css">
@@ -5,16 +41,16 @@
     <script src="https://kit.fontawesome.com/2a4e989807.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php include '../../generic-header.php'?>
-    <section class="content">
-        <form>
-            <label for="title">Title</label>
-            <input type="text" placeholder="Title me!">
-            <label for="description">Description</label>
-            <input type="textarea" placeholder="Describe the project">
-            <label for="link">Link</label>
-            <input type="url" placeholder="Link to your project's website">
-            <input type="submit" value="Add this card">
-        </form>
-    </section>
+<?php include '../../generic-header.php' ?>
+<section class="content">
+    <form>
+        <label for="title">Title</label>
+        <input type="text" placeholder="Title me!" value="<?php if ($_GET['method'] === "update") echo $row[4]; ?>">
+        <label for="description">Description</label>
+        <input type="textarea" value="<?php if ($_GET['method'] === "update") echo $row[3]; ?>">
+        <label for="link">Link</label>
+        <input type="url" placeholder="Link to your project's website">
+        <input type="submit" value="<?php if ($_GET['method'] === "update") echo $row[2] ?>">
+    </form>
+</section>
 </body>
--- user/index.php
+++ user/index.php
@@ -1,7 +1,7 @@
- <?php
+<?php
 session_start();
 
-if(!$_SESSION['logged_in']) {
+if (!$_SESSION['logged_in']) {
     echo "<script>window.location='../login/index.php';</script>";
 }
 
@@ -19,18 +19,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $search_param = $_GET['uid'];
     $QUERY = $connection->prepare("SELECT * FROM USERS WHERE user_id LIKE ?");
     $QUERY->bind_param("i", $search_param);
-    if($QUERY->execute) {
+    if ($QUERY->execute) {
         $cards = "";
         $results = $QUERY->get_result();
-        if($results->num_rows == 1) {
+        if ($results->num_rows == 1) {
             $row = $results->fetch_row();
             $CARD_QUERY = $connection->prepare("SELECT * FROM WEBPAGEDATA WHERE user_id LIKE " . $row[0]);
-            if($CARD_QUERY->execute) {
+            if ($CARD_QUERY->execute) {
                 $results2 = $CARD_QUERY->get_result();
-                if($results2->num_rows > 0) {
-                    while($row = $results2->fetch_row()) {
-                        if($_SESSION['uid'] === $row[1])
-                        $cards .= "<link rel=\"stylesheet\" href=\"/user/card.css\" type=\"text/css\">
+                if ($results2->num_rows > 0) {
+                    while ($row = $results2->fetch_row()) {
+                        if ($_SESSION['uid'] === $row[1]) {
+                            $cards .= "<link rel=\"stylesheet\" href=\"/user/card.css\" type=\"text/css\">
+<script src=\"https://kit.fontawesome.com/2a4e989807.js\" crossorigin=\"anonymous\"></script>
+<div class=\"card-wrapper\">
+    <a href=\"add-card/index.php?card_id=$row[0]&method=update\" class=\"edit\"><i class=\"fas fa-edit\"></i></a>
+    <a href=\"add-card/index.php?card_id=$row[0]&method=delete\" class=\"delete\"><i class=\"fas fa-trash\"></i></a>
+    <a href=\"$row[2]\">
+    <article>
+        <img class=\"label\" src=\"/images/banner3.png\" height=\"240px\" width=\"360px\">
+        <section class=\"card-body\">
+            <h1>$row[4]</h1>
+            <p>$row[3]</p>
+        </section>
+    </article>
+    </a>
+</div>
+";
+                        } else {
+                            $cards .= "<link rel=\"stylesheet\" href=\"/user/card.css\" type=\"text/css\">
 <div class=\"card-wrapper\">
     <a href=\"" . $row[2] . "\">
     <article>
@@ -43,6 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     </a>
 </div>
 ";
+                        }
                     }
                 }
             }
@@ -58,9 +76,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     <script src="https://kit.fontawesome.com/2a4e989807.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php include '../generic-header.php'?>
-    <section class="content">
-        <?php echo $cards; ?>
-        <a id="add-card" href="add-card"><i class="fas fa-plus-circle"></i></a>
-    </section>
+<?php include '../generic-header.php' ?>
+<section class="content">
+    <?php echo $cards; ?>
+    <a id="add-card" href="add-card"><i class="fas fa-plus-circle"></i></a>
+</section>
 </body>
