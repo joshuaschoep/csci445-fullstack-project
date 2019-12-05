1c71c43e8fc226e7692cfe529252ee1effe7c569
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 18:55:32 -0700

    updated index

--- login/signup_verify.php
+++ login/signup_verify.php
@@ -18,13 +18,15 @@ if(isset($_GET["e"]) && !empty($_GET['e']) AND isset($_GET["h"]) && !empty($_GET
     $user_query = "SELECT user_id,hash FROM USERS WHERE email=$email";
     $check_hash = $conn->query($user_query)->fetch_assoc()["hash"];
     $user_id = $conn->query($user_query)->fetch_assoc()["user_id"];
-    if ($hash === $check_hash) {
+    echo 'check hash: ' . $check_hash . "<br>";
+    echo 'email: ' . $email . "<br>"; 
+    if ($hash == $check_hash) {
         // prepare update statement and execute
-        $update_user_active = $conn->prepare("UPDATE USERS SET active=? WHERE email=?");
-        $update_user_active->bind_param("ii", $uid, $a);
-
-        $uid = $user_id;
+        $update_user_active = $conn->prepare("UPDATE USERS SET active=? WHERE user_id=?");
+        $update_user_active->bind_param("ii", $a, $uid);
+        
         $a = 1;
+        $uid = $user_id;
         $update_user_active->execute();
         $update_user_active->close();
     }
