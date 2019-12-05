fe608e80da984eacd89ec818141988ac40e2e4f6
Tanner Lucas <tannerlucas1996@gmail.com>
2019-12-04 23:44:32 -0700

    done

--- login/login.php
+++ login/login.php
@@ -1,12 +1,16 @@
 <link rel="stylesheet" href="login.css" type="text/css">
-<form id="login">
+<script src="./sha512.js"></script>
+<script src="./signup.js"></script>
+<form id="login" action="../index.php" onsubmit="return loginInfo()">
     <fieldset>
         <h4>Log in</h4>
         <div class="email">
-            <label for="email"><i class="fas fa-at"></i></label><input type="email" name="email" placeholder="Email Address or Username">
+            <label for="email"><i class="fas fa-at"></i></label>
+            <input id="uid_email" type="email" name="email" placeholder="Email Address or Username">
         </div>
         <div class="password">
-            <label for="password"><i class="fas fa-lock"></i></label><input type="password" name="password" placeholder="Password">
+            <label for="password"><i class="fas fa-lock"></i></label>
+            <input id="upass" type="password" name="password" placeholder="Password">
         </div>
         <div class="submit">
             <input type="submit" value="Submit"></input>
--- login/signup.js
+++ login/signup.js
@@ -25,6 +25,7 @@ function checkPasswordsMatch() {
 var valid_username = false;
 
 function checkUserInfo() {
+    p1 = document.getElementById("pw").value;
     if (checkPasswordsMatch()) {
         if (valid_username == false) {
             alert("Username already taken. please try again");
@@ -47,40 +48,93 @@ function checkUserInfo() {
 function checkUserName() {
     var xht;
     
-        var uname = "";
-        uname = document.getElementById("uname").value;
-        console.log('uname: '+uname);
-        // query for usernames and if it exists reset the page
-        if (window.XMLHttpRequest) {
-            xht = new XMLHttpRequest();
-        } else {
-            xht = new ActiveXObject("Microsoft.XMLHTTP");
-        }
+    var uname = "";
+    uname = document.getElementById("uname").value;
+    console.log('uname: '+uname);
+    // query for usernames and if it exists reset the page
+    if (window.XMLHttpRequest) {
+        xht = new XMLHttpRequest();
+    } else {
+        xht = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
-        xht.onreadystatechange = stateChanged;
-        xht.open('GET', 'check_user.php?uname='+uname,true);
-        xht.send();
-        
-        function stateChanged() {
-            if(xht.readyState==4 && xht.status==200) {
-                if (xht.responseText == "repeat") {
-                    // console.log("resp.val onreadystatechange before: "+resp.val);
-                    // update global to reference onsubmit...
-                    valid_username = false;
-
-                    // if the username is a repeat, return false
-                    // alert("Username already taken. please try again");
-                    // document.getElementById("uname").value = "";
-                    return false;
-                    }
-                    
+    xht.onreadystatechange = stateChanged;
+    xht.open('GET', 'check_user.php?uname='+uname,true);
+    xht.send();
+    
+    function stateChanged() {
+        if(xht.readyState==4 && xht.status==200) {
+            console.log("returned text: "+xht.responseText);
+            if (xht.responseText == "repeat") {
+                // console.log("resp.val onreadystatechange before: "+resp.val);
+                // update global to reference onsubmit...
                 
-                else {
-                    // no matches
-                    valid_username = true; 
-                    return true;
+                valid_username = false;
+                console.log("on state change valid username: "+valid_username);
+                // if the username is a repeat, return false
+                // alert("Username already taken. please try again");
+                // document.getElementById("uname").value = "";
+                return false;
                 }
+                
+            
+            else {
+                // no matches
+                valid_username = true; 
+                return true;
             }
+        }
+        return false;
+    }
+}
+
+var valid_login = false;
+
+function loginInfo() {
+    checkSignInInfo()
+    if (valid_login == false) {
+        alert("incorrect login credentials");
+        document.getElementById("login").reset();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function checkSignInInfo() {
+    var xht;
+    
+    var uid_email = "";
+    var upass = "";
+    uid_email = document.getElementById("uid_email").value;
+    upass = document.getElementById("upass").value;
+    upass = hex_sha512(upass);
+    // console.log('uname: '+uname);
+    // query for usernames and if it exists reset the page
+    if (window.XMLHttpRequest) {
+        xht = new XMLHttpRequest();
+    } else {
+        xht = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xht.onreadystatechange = submitLogin;
+    xht.open('GET', 'check_login.php?uid_email='+uid_email+'&upass='+upass,true);
+    xht.send();
+
+    function submitLogin() {
+        if(xht.readyState==4 && xht.status==200) {
+            if (xht.responseText == "success") {
+                valid_login = true;
+            }
+            else {
+                valid_login = false;
+            }
+        }
+        else {
+            // bad response
             return false;
         }
+    }
+
 }
