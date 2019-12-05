2883af12fc154ee3d71570030d6777f70b3a7b3a
Josh Schoep <joshuaschoep@gmail.com>
2019-12-04 17:35:18 -0700

    Added cards

---
+++ user/card.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="/user/card.css" type="text/css">
+<div class="card-wrapper">
+    <a href="">
+    <article>
+        <img class="label" src="" height="180px" width="480px">
+        <section class="card-body">
+            <h1>Title</h1>
+            <p>Describe me!</p>        
+        </section>
+    </article>
+    </a>
+</div>
